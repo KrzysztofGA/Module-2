7b6c703d33d8c216af6968b34e50d3e0540d0d90
KrzysztofGA <krzysztof@getacademy.no>
2020-05-13 08:36:41 +0200

    Gjort oppgave 2

--- oppgaver/2-objekter-med-egne-felt/README.md
+++ oppgaver/2-objekter-med-egne-felt/README.md
@@ -8,14 +8,18 @@
 ### Polski
 
 1. Dodaj nową kolumnę dla osoby. Musi być możliwe określenie podczas tworzenia nowego zadania i musi być możliwe do edycji.
-1. Dodaj nową kolumnę dla terminu. Użyj znacznika wejściowego `type="date"`. Konwersja formatu wyświetlania: `new Date("2019-02-13").toLocaleDateString()`. Aby utworzyć inną datę we właściwym formacie `<input type="date", bruk myDate.toISOString().substr(0,10)`
+
+1. Dodaj nową kolumnę dla terminu. Użyj znacznika wejściowego `type="date"`. Konwersja formatu wyświetlania: `new Date("2019-02-13").toLocaleDateString()`. Aby utworzyć datę we właściwym formacie `<input type="date" />`, posłuż się `myDate.toISOString().substr(0,10)`
+
 1. Dodaj nową kolumnę „data wykonania”, która zostanie wypełniona automatycznie po naciśnięciu pola wyboru, aby to zrobić.
 
 ### Norsk
 
 1. Legg til en ny kolonne for person. Må kunne angis når man lager ny oppgave og må kunne redigeres
-1. Legg til en ny kolonne for frist. Bruk input-tag med `type="date"`. Konvertering til visningsformat: `new Date("2019-02-13").toLocaleDateString()`. For å lage en dato på riktig format til `<input type="date", bruk myDate.toISOString().substr(0,10)`
+
+1. Legg til en ny kolonne for frist. Bruk input-tag med `type="date"`. Konvertering til visningsformat: `new Date("2019-02-13").toLocaleDateString()`. For å lage en dato på riktig format til `<input type="date" />`, bruk `myDate.toISOString().substr(0,10)`
+
 1. Legg til en ny kolonne for "gjort dato" som fylles ut automatisk når man trykker på checkboxen for at det er gjort.
 
 [1]: https://krzysztofga.github.io/GetAcademy/Module-2/oppgaver/2-objekter-med-egne-felt
-[2]: https://github.com/KrzysztofGA/GetAcademy/tree/master/GetAcademy/Module-2/oppgaver/2-objekter-med-egne-felt
+[2]: https://github.com/KrzysztofGA/GetAcademy/tree/master/Module-2/oppgaver/2-objekter-med-egne-felt
--- oppgaver/2-objekter-med-egne-felt/script.js
+++ oppgaver/2-objekter-med-egne-felt/script.js
@@ -1,33 +1,60 @@
 // Model
-var tasks = [
-    {description: 'Handle til middag', isDone: true},
-    {description: 'Lage middag', isDone: false},
-    {description: 'Spise middag', isDone: false},
+const tasks = [
+    {
+        description: 'Handle til middag',
+        person: 'Adam',
+        date: '2020-03-23',
+        isDone: true,
+        completed: '2020-03-27',
+    },
+    {
+        description: 'Lage middag',
+        person: 'Peter',
+        date: '2020-03-24',
+        isDone: false,
+        completed: '',
+    },
+    {
+        description: 'Spise middag',
+        person: 'Axel',
+        date: '2020-03-25',
+        isDone: false,
+        completed: '',
+    },
 ];
 
 // Controller
-var taskDescriptionInput = document.getElementById('taskDescription');
+const taskDescriptionInput = document.getElementById('taskDescription');
+const personDescriptionInput = document.getElementById('personDescription');
+const dateDescriptionInput = document.getElementById('dateDescription');
 
 function addTask() {
     tasks.push({
         description: taskDescriptionInput.value,
+        person: personDescriptionInput.value,
+        date: dateDescriptionInput.value,
         isDone: false,
+        completed: '',
     });
     taskDescriptionInput.value = '';
+    personDescriptionInput.value = '';
+    dateDescriptionInput.value = '';
     show();
 }
 
 // View
-var tasksTable = document.getElementById('tasksTable');
+const tasksTable = document.getElementById('tasksTable');
 show();
 
 function show() {
-    let html = `
-                        <tr>
-                            <th>Oppgave</th>
-                            <th>Gjort</th>
-                            <th></th>
-                        </tr>`;
+    let html = `<tr>
+                    <th>Oppgave</th>
+                    <th>Person</th>
+                    <th>Frist</th>
+                    <th>Gjort</th>
+                    <th>Gjort dato</th>
+                    <th></th>
+                </tr>`;
     for (let i = 0; i < tasks.length; i++) {
         html += createHtmlRow(i);
     }
@@ -39,26 +66,33 @@ function createHtmlRow(i) {
     const checkedHtml = task.isDone ? 'checked="checked"' : '';
     if (!task.editMode)
         return `<tr>
-                            <td>${task.description}</td>
-                            <td><input onchange="changeIsDone(this, ${i})" type="checkbox" ${checkedHtml} /></td>
-                            <td>
-                                <button onclick="deleteTask(${i})">Slett</button>
-                                <button onclick="editTask(${i})">Rediger</button>
-                            </td>
-                        </tr>
-                        `;
+                    <td>${task.description}</td>
+                    <td>${task.person}</td>
+                    <td>${task.date}</td>
+                    <td><input onchange="changeIsDone(this, ${i})" type="checkbox" ${checkedHtml} /></td>
+                    <td>${task.completed}</td>
+                    <td>
+                        <button onclick="deleteTask(${i})">Slett</button>
+                        <button onclick="editTask(${i})">Rediger</button>
+                    </td>
+                </tr>`;
     return `<tr>
-                            <td><input id="editDescription${i}" type="text" value="${task.description}"/></td>
-                            <td><input onchange="changeIsDone(this, ${i})" type="checkbox" ${checkedHtml} /></td>
-                            <td>
-                                <button onclick="updateTask(${i})">Lagre</button>
-                            </td>
-                        </tr>
-                        `;
+                <td><input id="editDescription${i}" type="text" value="${task.description}"/></td>
+                <td><input id="editPerson${i}" type="text" value="${task.person}"/></td>
+                <td><input id="editDate${i}" type="date" value="${task.date}"/></td>
+                <td><input onchange="changeIsDone(this, ${i})" type="checkbox" ${checkedHtml} /></td>
+                <td><input id="editCompleted${i}" type="date" value="${task.completed}"/></td>
+                <td>
+                    <button onclick="updateTask(${i})">Lagre</button>
+                </td>
+            </tr>`;
 }
 
 function changeIsDone(checkbox, index) {
     tasks[index].isDone = checkbox.checked;
+    checkbox.checked
+        ? (tasks[index].completed = new Date().toISOString().substr(0, 10))
+        : (tasks[index].completed = '');
     show();
 }
 
@@ -73,10 +107,19 @@ function editTask(index) {
 }
 
 function updateTask(index) {
-    const id = `editDescription${index}`;
-    const inputTag = document.getElementById(id);
+    const idDescription = `editDescription${index}`;
+    const inputDescritpion = document.getElementById(idDescription);
+    const idPerson = `editPerson${index}`;
+    const inputPerson = document.getElementById(idPerson);
+    const idDate = `editDate${index}`;
+    const inputDate = document.getElementById(idDate);
+    const idCompleted = `editCompleted${index}`;
+    const inputCompleted = document.getElementById(idCompleted);
     const task = tasks[index];
-    task.description = inputTag.value;
+    task.description = inputDescritpion.value;
+    task.person = inputPerson.value;
+    task.date = inputDate.value;
     task.editMode = false;
+    task.completed = inputCompleted.value;
     show();
 }
